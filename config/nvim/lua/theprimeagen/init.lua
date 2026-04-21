6ceea748e15fd010c7b93e791dfac2bb4efc8637
Jakub Guštafik <ja.github@gustafik.com>
2026-04-21 11:53:21 +0200

    Add dotnet-ls support

--- config/nvim/lua/theprimeagen/init.lua
+++ config/nvim/lua/theprimeagen/init.lua
@@ -64,6 +64,14 @@ autocmd('LspAttach', {
     end
 })
 
+autocmd('VimEnter', {
+    group = ThePrimeagenGroup,
+    callback = function(e)
+        -- C# LSP needs this to find dotnet installed via homebrew
+        vim.env.DOTNET_ROOT = "/opt/homebrew/opt/dotnet/libexec"
+    end
+})
+
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
 vim.g.netrw_winsize = 25
